b42552181fc070b11bd20bdc8b547923eb65a079
lfjshuaige <15770735296@139.com>
2020-06-23 14:20:39 +0800

    新增eureka组件

--- microservicecloud-provider-dept-8001/src/main/java/com/lfj/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/lfj/springcloud/DeptProvider8001_App.java
@@ -2,8 +2,11 @@ package com.lfj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//本服务启动后会自动注册进eureka服务中
+@EnableEurekaClient
 public class DeptProvider8001_App {
 
     public static void main(String[] args) {
